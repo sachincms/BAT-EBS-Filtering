2ab12825d4c1d30b736420b43c1804d8d4c8b381
sachincms <sachin.r@catalysts.org>
2025-05-14 12:23:55 +0530

    added constants

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 import sys
-sys.path.append('.')
+current_file_path = os.path.abspath(__file__)
+parent_directory = os.path.dirname(current_file_path)
+sys.path.append(parent_directory)
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,3 +11,6 @@ MONGODB_URI = os.getenv('MONGODB_URI')
 MONGODB_DATABASE = os.getenv('MONGODB_DATABASE')
 MONGODB_SCRAPED_COLLECTION = os.getenv('MONGODB_SCRAPED_COLLECTION')
 MONGODB_PROCESSED_COLLECTION = os.getenv('MONGODB_PROCESSED_COLLECTION')
+MONGODB_UNIQUE_KEY = os.getenv('MONGODB_UNIQUE_KEY')
+
+CONTENT_UNAVAILABLE_MESSAGE = "Content unavailable"
