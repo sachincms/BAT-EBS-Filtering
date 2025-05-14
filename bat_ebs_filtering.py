6cd18f755a393ab6c31e2b7995e10730fd97d34e
sachincms <sachin.r@catalysts.org>
2025-05-14 12:42:10 +0530

    removed duplicate checks since it is implemented in the handler

--- bat_ebs_filtering.py
+++ bat_ebs_filtering.py
@@ -1,34 +1,18 @@
 import streamlit as st
-from datetime import datetime
-from pymongo import MongoClient
-from pymongo.server_api import ServerApi
-from config import MONGODB_URI, MONGODB_DATABASE, MONGODB_SCRAPED_COLLECTION, MONGODB_PROCESSED_COLLECTION
+from utils.db_loader import load_to_mongodb
+from utils.retrieve_data import retrieve
+from config import CONTENT_UNAVAILABLE_MESSAGE
 
-client = MongoClient(MONGODB_URI, server_api=ServerApi('1'))
-db = client[MONGODB_DATABASE]
-scraped_collection = db[MONGODB_SCRAPED_COLLECTION]
-processed_collection = db[MONGODB_PROCESSED_COLLECTION]
-
-start_date = datetime.strptime('25/02/2025 00:00:00.0', '%d/%m/%Y %H:%M:%S.%f')
-end_date = datetime.today()
-
-query = {
-    'scraped_date': {
-        '$gte': start_date,
-        '$lte': end_date
-    }
-}
-documents = scraped_collection.find(query)
-documents = list(documents)
+documents, existing_ids = retrieve()
 
 def display_documents(documents, relevant_list):
     st.title("Document Relevance")
     st.subheader("Select relevant documents")
+    st.write(f"Total documents: {len(documents)}")
 
     already_displayed = []
+    total_inserted_count = 0
     for idx, doc in enumerate(documents):
-        if doc['scraped_from'] == 'Bangalore Mirror':
-            continue
         if idx > 0 and idx % 50 == 0:
             if st.button("Count", key=f"{idx}_count_button"):
                 st.write("Relevant list: ", relevant_list)
@@ -38,16 +22,16 @@ def display_documents(documents, relevant_list):
 
             if st.button("Proceed", key=f"{idx}_proceed_button"):
                 relevant_documents = [doc for idx, doc in enumerate(documents) if idx in relevant_list]
-                for doc in relevant_documents:
+                non_duplicate_documents = [doc for doc in relevant_documents if doc['_id'] not in existing_ids]
+                duplicate_document_count = len(relevant_documents) - len(non_duplicate_documents)
+                load_to_mongodb(non_duplicate_documents)
 
-                    try:
-                        existing_doc = processed_collection.find_one({'_id': doc['_id']})
-                        if existing_doc is None:
-                            processed_collection.insert_one(doc)
-                    except Exception as ex:
-                        print(ex)
-                        continue
-                st.write(f"Inserted {len(relevant_documents)} relevant documents")
+                st.write(f"Selected documents: {len(relevant_documents)}")
+                st.write(f"Inserted documents: {len(non_duplicate_documents)}")
+                if duplicate_document_count > 0:
+                    st.write(f"The remaining {duplicate_document_count} documents are duplicates.")
+
+                total_inserted_count += len(non_duplicate_documents)
                 relevant_list = []
             
             st.divider()
@@ -75,12 +59,18 @@ def display_documents(documents, relevant_list):
 
     if st.button("Proceed", key="final_proceed_button"):
         relevant_documents = [doc for idx, doc in enumerate(documents) if idx in relevant_list]
-        for doc in relevant_documents:
-            existing_doc = processed_collection.find_one({'_id': doc['_id']})
-            if existing_doc is None:
-                processed_collection.insert_one(doc)
+        non_duplicate_documents = [doc for doc in relevant_documents if doc['_id'] not in existing_ids]
+        duplicate_document_count = len(relevant_documents) - len(non_duplicate_documents)
+        load_to_mongodb(non_duplicate_documents)
+
+        st.write(f"Selected documents: {len(relevant_documents)}")
+        st.write(f"Inserted documents: {len(non_duplicate_documents)}")
+        if duplicate_document_count > 0:
+            st.write(f"The remaining {duplicate_document_count} documents are duplicates.")
+        total_inserted_count += len(non_duplicate_documents)
+
         with st.container(border=True):
-            st.header(f"Added {len(relevant_documents)} to the database. You may close the app now.")
+            st.header(f"Added a total of {len(total_inserted_count)} to the database. You may close the app now.")
 
 relevant_list = []
 display_documents(documents, relevant_list)
